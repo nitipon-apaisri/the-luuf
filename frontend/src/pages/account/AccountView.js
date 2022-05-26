341345206017f7bb67e18a342ed6fade452a374f
nitipon apaisri <nitipon.apaisri@gmail.com>
2022-05-26 10:22:22 +0200

    Fixed none auth at account view

--- frontend/src/pages/account/AccountView.js
+++ frontend/src/pages/account/AccountView.js
@@ -9,6 +9,7 @@ import Collection from "../../components/account/collection/Collection";
 import Favorited from "../../components/account/favorited/Favorited";
 import { accounts } from "../../db";
 import { InstagramFilled, LinkOutlined, SettingFilled, TwitterOutlined } from "@ant-design/icons";
+import axios from "axios";
 const AccountPage = () => {
     const { accountName } = useParams();
     const accountContext = useContext(AccountContext);
@@ -30,11 +31,23 @@ const AccountPage = () => {
     useEffect(() => {
         document.title = `${accountName} - creation`;
         setLoader(true);
-        setTimeout(() => {
-            setAccount(accountContext.account);
-        }, 750);
+        if (isAuth === false && account === undefined) {
+            setTimeout(() => {
+                axios({
+                    method: "get",
+                    url: `http://localhost:4200/wallet/${accountName}`,
+                })
+                    .then((wallet) => {
+                        setAccount(wallet.data);
+                    })
+                    .catch((err) => {
+                        console.log(err.response.data.error);
+                    });
+                setAccount(accountContext.account);
+            }, 750);
+        }
         if (account !== undefined) setLoader(false);
-    }, [accountContext, accountName, account]);
+    }, [accountContext, accountName, account, isAuth]);
     return (
         <MainLayout>
             {loader && (
@@ -111,16 +124,16 @@ const AccountPage = () => {
                         <Divider />
                         {(() => {
                             if (tokenCategory === "creation") {
-                                return <Creation accountName={account.name} />;
+                                return <Creation accountName={account.signInInfo.walletAddress} />;
                             }
                             if (tokenCategory === "collectible") {
-                                return <Collectible accountName={account.name} />;
+                                return <Collectible accountName={account.signInInfo.walletAddress} />;
                             }
                             if (tokenCategory === "collection") {
-                                return <Collection accountName={account.name} />;
+                                return <Collection accountName={account.signInInfo.walletAddress} />;
                             }
                             if (tokenCategory === "favorited") {
-                                return <Favorited account={account} />;
+                                return <Favorited account={account.signInInfo.walletAddress} />;
                             }
                         })()}
                     </div>
